1703e6e37d97a9363a8cb306fcfe98b31a44d29d
ririyad <ririyad.291@gmail.com>
2017-07-18 15:27:14 +0600

    GFETCH-432 removed unecessary namespaces

--- NerdCats.PortWallet/Request/PaymentVerify.cs
+++ NerdCats.PortWallet/Request/PaymentVerify.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NerdCats.PortWallet.Request
+﻿namespace NerdCats.PortWallet.Request
 {
     public class PaymentVerify
     {
         public string call => "ipn_validate";
-        public double amount { get; set; }
+        public double Amount { get; set; }
         public InvoiceRequest Invoice { get; set; }
     }
 }
