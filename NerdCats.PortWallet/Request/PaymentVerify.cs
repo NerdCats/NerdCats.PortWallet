6b822a21686a570a55c34b22aa38c9cd8ceb9053
ririyad <ririyad.291@gmail.com>
2017-07-18 15:25:09 +0600

    GFETCH-432 added class to verify payment

---
+++ NerdCats.PortWallet/Request/PaymentVerify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdCats.PortWallet.Request
+{
+    public class PaymentVerify
+    {
+        public string call => "ipn_validate";
+        public double amount { get; set; }
+        public InvoiceRequest Invoice { get; set; }
+    }
+}
